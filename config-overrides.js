25615a8f88655910c7b8ca9b3dcc9fd74fc7d19a
cityhunter <dongjun1997@outlook.com>
2022-03-02 20:41:51 +0800

    feat: 配置全局scss变量

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const {override, adjustStyleLoaders} = require("customize-cra");
+module.exports = override(
+    adjustStyleLoaders(rule => {
+      if (rule.test.toString().includes("sass")) {
+        rule.use.push({
+          loader: "sass-resources-loader",
+          options: {
+            resources: [
+              "./src/styles/variables.scss",
+              "./src/styles/mixins.scss",
+            ],
+          },
+        });
+      }
+    })
+);
