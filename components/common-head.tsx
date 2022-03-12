df3e1d656beea1da781d64001dca38b458ce6d5a
cityhunter <dongjun1997@outlook.com>
2022-03-12 16:32:38 +0800

    feat: 添加谷歌广告

--- components/common-head.tsx
+++ components/common-head.tsx
@@ -13,6 +13,8 @@ export default function CommonHead(props: {title?: string, description?: string}
         <meta name="msvalidate.01" content="AB78A0C987D3AEA34B969889512FF33E" />
         {/*谷歌验证*/}
         <meta name="google-site-verification" content="4rS5gjy4-tFOdJO4HvDg0_E4QNcB1D1YeZo29iS4hIY" />
+        {/*谷歌广告*/}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1031326139722537" crossOrigin="anonymous"/>
       </Head>
   );
 }
