6039a629dab2dbc92dd51cb7f5298a3a340a26e8
cityhunter <dongjun1997@outlook.com>
2022-03-12 15:20:00 +0800

    fix: 修复无法 build bug

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactMarkdown from 'react-markdown';
 import style from '../../styles/blog.module.scss';
 import CommonHead from "../../components/common-head";
@@ -9,15 +9,28 @@ import {useRouter} from "next/router";
  * @return {React.ReactElement}
  */
 export default function BlogDetail() {
+  // const [blog, setBlog] = React.useState({title: '', description: '', mark_content: ''});
+  let blog = {title: '', description: '', mark_content: ''};
   const {id} = useRouter().query;
-  const blog = require('../../src/assets/data/blog.json').find((item: any) => item.id === id);
+  if (id) {
+    const blogs: any[] = require('../../src/assets/data/blog.json');
+    blog = blogs.find((item: any) => item.id === id);
+  }
+  // useEffect(() => {
+  //   console.log(blogs);
+  //   console.log(id);
+  //   console.log(typeof id);
+  //   console.log(blogs.find(item => item.id === id));
+  //   setBlog(blogs.find(item => item.id === id));
+  //   // setBlog(require('../../src/assets/data/blog.json').find((item: any) => item.id === id));
+  // }, [])
   return (
       <div>
-        <CommonHead title={blog.title} description={blog.dependencies}/>
+        <CommonHead title={blog.title} description={blog.description}/>
         <div className={style['blog-detail']}>
           <div className={style['blog-content']}>
             <ReactMarkdown>
-              {blog.mark_content ? blog.mark_content : ''}
+              {blog.mark_content}
             </ReactMarkdown>
           </div>
         </div>
