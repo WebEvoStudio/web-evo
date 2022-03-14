d702299ba4a9e45eed5510b1dde942661915cc98
city-hunter <dongjun1997@outlook.com>
2022-03-14 19:36:18 +0800

    fix: 修改 Google Analytics id

--- components/common-head.tsx
+++ components/common-head.tsx
@@ -28,7 +28,7 @@ export default function CommonHead(props: {title?: string, description?: string}
       {/* 百度联盟验证*/}
       <meta name="baidu_union_verify" content="50312ad0c63983b3fe6c2e2a6e10c386"/>
       {/* Google Analytics*/}
-      <Script src={'https://www.googletagmanager.com/gtag/js?id=G-KZY93Q4TYH'} async={true}/>
+      <Script src={'https://www.googletagmanager.com/gtag/js?id=G-YKCPHDYK53'} async={true}/>
       <Script dangerouslySetInnerHTML={{
         __html: `
         window.dataLayer = window.dataLayer || [];
