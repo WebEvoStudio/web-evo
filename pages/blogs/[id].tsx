47e65bad6611ac8b1c43ffc58d68c567b6eb54f4
city-hunter <dongjun1997@outlook.com>
2022-03-21 18:04:37 +0800

    fix: 优化文章内部显示

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -4,25 +4,28 @@ import style from '../../styles/blog.module.scss';
 import CommonHead from '../../components/common-head';
 import axios from 'axios';
 import Markdown from '../../core/unit/markdown';
+import {Container} from '@mui/material';
 /**
  * blog detail page
  * @return {React.ReactElement}
  */
 function BlogDetail({blog}: {blog: {title: string, mark_content: string}}) {
   return (
-    <div className={style['blog']}>
-      <CommonHead title={blog.title} description={Markdown.intercept(blog.mark_content, 100)}/>
-      <div className={style['blog-detail']}>
-        <div className={style['blog-title']}>
-          <h1>{blog.title}</h1>
-        </div>
-        <div className={style['blog-content']}>
-          <ReactMarkdown>
-            {blog.mark_content}
-          </ReactMarkdown>
+    <Container maxWidth={'md'}>
+      <div className={style['blog']}>
+        <CommonHead title={blog.title} description={Markdown.intercept(blog.mark_content, 100)}/>
+        <div className={style['blog-detail']}>
+          <div className={style['blog-title']}>
+            <h1>{blog.title}</h1>
+          </div>
+          <div className={style['blog-content']}>
+            <ReactMarkdown>
+              {blog.mark_content}
+            </ReactMarkdown>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
 BlogDetail.getInitialProps = async (ctx: any) => {
