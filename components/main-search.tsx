c68899326295932141975435b905e4671467ed95
cityhunter <dongjun1997@outlook.com>
2022-03-27 05:21:08 +0800

    fix: 恢复

---
+++ components/main-search.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {NextPage} from 'next';
+import {Search as SearchIcon} from '@mui/icons-material';
+import {alpha, InputBase, styled} from '@mui/material';
+import {useRouter} from 'next/router';
+
+const MainSearch: NextPage = () => {
+  const router = useRouter();
+  const Search = styled('div')(({theme}) => ({
+    'position': 'relative',
+    'borderRadius': theme.shape.borderRadius,
+    'backgroundColor': alpha(theme.palette.common.white, 0.15),
+    '&:hover': {backgroundColor: alpha(theme.palette.common.white, 0.25)},
+    'marginRight': theme.spacing(2),
+    'marginLeft': 0,
+    'width': '100%',
+    [theme.breakpoints.up('sm')]: {marginLeft: theme.spacing(3), width: 'auto'},
+  }));
+  const SearchIconWrapper = styled('div')(({theme}) => ({
+    position: 'absolute',
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  const StyledInputBase = styled(InputBase)(({theme}) => ({
+    'color': 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+
+      <StyledInputBase
+        placeholder="Index…"
+        inputProps={{'aria-label': 'search'}}
+        onKeyDown={({key, target}) => {
+          const {value} = target as HTMLInputElement;
+          if (key === 'Enter' && value) router.push(`/search?query=${value}`).then();
+        }}
+      />
+    </Search>
+  );
+};
+export default MainSearch;
---
+++ pages/search/index.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import {NextPage} from 'next';
+import {Container, Paper, Skeleton} from '@mui/material';
+import BlogRequests from '../../core/requests/blog.requests';
+import Link from 'next/link';
+import styles from '../../styles/blog.module.scss';
+import Markdown from '../../core/unit/markdown';
+import {useRouter} from 'next/router';
+const Index: NextPage = () => {
+  const {query} = useRouter().query;
+  const [blogs, setBlogs] = useState<any[]>([]);
+  useEffect(() => {
+    if (query) {
+      BlogRequests.search(query.toString()).then((res) => {
+        setBlogs(res);
+      });
+    }
+  }, [query]);
+  return (
+    <div>
+      <Container maxWidth={'md'}>
+        <h1>Search</h1>
+        {blogs.map((blog, index) => (
+          <Link href={`/blogs/${blog['_id']}`} key={index}>
+            <Paper elevation={1} sx={{margin: '10px 0'}}>
+              <div className={styles['blog-item']} key={index}>
+                <span className={styles['blog-title']}>{blog.title}</span>
+                <div className={styles['blog-description']}>
+                  {Markdown.intercept(blog.mark_content, 155)}
+                </div>
+                <Skeleton variant="rectangular"/>
+              </div>
+            </Paper>
+          </Link>
+        ))}
+      </Container>
+    </div>
+  );
+};
+export default Index;
