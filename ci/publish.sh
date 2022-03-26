f84a1a55c62e53040a58f583343965b8f4cf52d9
cityhunter <dongjun1997@outlook.com>
2022-03-26 22:38:47 +0800

    fix: 优化ci执行顺序防止打包不成功删除当前容器

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,6 +1,8 @@
 CONTAINER_ID=`docker ps -a -q -f name=next-app`
 IMAGE_ID=$(docker images | grep "next-app" | awk '{print $3}')
 echo $CONTAINER_ID
+echo "开始 next-app 镜像打包"
+docker build -t next-app .
 echo "停止 next-app"
 if [ -n "$CONTAINER_ID" ]
 then
@@ -13,7 +15,5 @@ then
   echo "删除 next-app 镜像"
   docker rmi $IMAGE_ID
 fi
-echo "开始 next-app 镜像打包"
-docker build -t next-app .
 echo "运行 next-app 镜像：4206"
 docker run --name next-app -p 4206:3000 -d next-app
