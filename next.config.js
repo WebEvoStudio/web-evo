7d8427e0208a336100dbcc60bee31c190c9f100b
cityhunter <dongjun1997@outlook.com>
2022-03-27 05:41:09 +0800

    fix: 移除pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+// const withPWA = require('next-pwa');
+// const runtimeCaching = require('next-pwa/cache');
 const path = require('path');
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -11,10 +11,10 @@ const nextConfig = withPWA({
     NEXT_PUBLIC_BASE_URL: process.env.BASE_URL,
     NEXT_PUBLIC_SITE_URL: process.env.SITE_URL,
   },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-});
+  // pwa: {
+  //   dest: 'public',
+  //   runtimeCaching,
+  // },
+};
 
 module.exports = nextConfig;
