8c4c75544d51f498ba332417c16d80f8297406cd
cityhunter <dongjun1997@outlook.com>
2022-03-31 00:24:10 +0800

    feat: 添加 microsoft clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import {CssBaseline, ThemeProvider} from '@mui/material';
 import theme from '../core/themes/theme';
 import {CacheProvider, EmotionCache} from '@emotion/react';
 import createEmotionCache from '../core/unit/createEmotionCache';
+import Script from 'next/script';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -28,6 +29,16 @@ function MyApp(props: MyAppProps) {
         <BasicLayout>
           {/* Google Analytics*/}
           <GoogleAnalytics measurementId={'G-YKCPHDYK53'}/>
+          {/* microsoft clarity */}
+          <Script dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "b91ge7rn7p");
+            `,
+          }}/>
           <CssBaseline/>
           <Component {...pageProps} />
         </BasicLayout>
