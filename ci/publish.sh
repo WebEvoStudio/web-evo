49ef874f864309568092a6740557d66c0ce19a5b
city-hunter <dongjun1997@outlook.com>
2022-04-14 18:14:33 +0800

    feat: 头条验证

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,19 +1,19 @@
-CONTAINER_ID=`docker ps -a -q -f name=next-app`
-IMAGE_ID=$(docker images | grep "next-app" | awk '{print $3}')
+CONTAINER_ID=`docker ps -a -q -f name=developer-center-web`
+IMAGE_ID=$(docker images | grep "developer-center-web" | awk '{print $3}')
 echo $CONTAINER_ID
-echo "停止 next-app"
+echo "停止 developer-center-web"
 if [ -n "$CONTAINER_ID" ]
 then
   docker stop $CONTAINER_ID
-  echo "删除 next-app 容器"
+  echo "删除 developer-center-web 容器"
   docker rm $CONTAINER_ID
 fi
 if [ -n "$IMAGE_ID" ]
 then
-  echo "删除 next-app 镜像"
+  echo "删除 developer-center-web 镜像"
   docker rmi $IMAGE_ID
 fi
-echo "开始 next-app 镜像打包"
-docker build -t next-app .
-echo "运行 next-app 镜像：4206"
-docker run --name next-app -p 4206:3000 -d next-app
+echo "开始 developer-center-web 镜像打包"
+docker build -t developer-center-web .
+echo "运行 developer-center-web 镜像：4206"
+docker run --name developer-center-web -p 4206:3000 -d developer-center-web
