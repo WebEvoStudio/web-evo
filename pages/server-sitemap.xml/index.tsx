6098e489de4057ae20cc88ee1afb5a10ae5d30d9
cityhunter <dongjun1997@outlook.com>
2022-04-17 01:50:38 +0800

    fix: 优化pwa表现形式

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -2,20 +2,16 @@ import {GetServerSideProps} from 'next';
 import {getServerSideSitemap} from 'next-sitemap';
 import axios from 'axios';
 
+const SitemapIndex = () => {};
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const fields: {loc: string, lastmod: string}[] = [];
   const host = 'http://developer.bulv.life';
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}api/blogs`;
   const response = await axios.get(url);
-  console.log(response.data);
   fields.push(...response.data.map((blog: any) => ({
     loc: `${host}/blogs/${blog['_id']}`,
     lastmod: new Date().toISOString(),
   })));
   return getServerSideSitemap(context, fields);
 };
-/**
- * server-sitemap.xml
- * @constructor
- */
-export default function SitemapIndex() {}
+export default SitemapIndex;
