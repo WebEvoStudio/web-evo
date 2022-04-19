657b149a3bcaa8f350cbbe14099a328df78cfcbd
city-hunter <dongjun1997@outlook.com>
2022-04-19 17:40:43 +0800

    fix: serve-sitemap.xml 500

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -4,14 +4,17 @@ import axios from 'axios';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const fields: {loc: string, lastmod: string}[] = [];
-  const host = 'http://developer.bulv.life';
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}api/blogs`;
-  const response = await axios.get(url);
-  console.log(response.data);
-  fields.push(...response.data.map((blog: any) => ({
-    loc: `${host}/blogs/${blog['_id']}`,
-    lastmod: new Date().toISOString(),
-  })));
+  try {
+    const host = 'http://developer.bulv.life';
+    const url = `${process.env.NEXT_PUBLIC_BASE_URL}api/blogs`;
+    const response = await axios.get(url);
+    fields.push(...response.data.map((blog: any) => ({
+      loc: `${host}/blogs/${blog['_id']}`,
+      lastmod: new Date().toISOString(),
+    })));
+  } catch (e) {
+    console.error(e);
+  }
   return getServerSideSitemap(context, fields);
 };
 /**
