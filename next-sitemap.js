2bf318e5ea6ee9e41bf3f931c2462173414e0202
city-hunter <dongjun1997@outlook.com>
2022-05-30 10:28:08 +0800

    fix: sitemap移除搜索页面

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,5 +3,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'http://developer.bulv.life',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ['/server-sitemap.xml', '/about', '/portfolio', '/contact', '/editor', '/blogs/publish'],
+  exclude: ['/server-sitemap.xml', '/about', '/portfolio', '/editor', '/blogs/publish', '/search'],
 };
