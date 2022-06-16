0cdb2179d090919d7b794ccd159bf04e08bcc751
city-hunter <dongjun1997@outlook.com>
2022-06-16 17:21:16 +0800

    feat: pwa 下 ui

--- layouts/basic.layout.tsx
+++ layouts/basic.layout.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from '../components/header';
+import {BottomNavigation, BottomNavigationAction, Paper} from '@mui/material';
+import {Home} from '@mui/icons-material';
+import {useRouter} from 'next/router';
 
 /**
  * basic.layout.tsx
@@ -8,10 +11,36 @@ import Header from '../components/header';
  * @return {JSX.Element}
  */
 export default function BasicLayout({children}: any) {
+  const router = useRouter();
+  const [isPwa, setIsPwa] = useState(false);
+  useEffect(() => {
+    setIsPwa(
+        ['fullscreen', 'standalone', 'minimal-ui'].some(
+            (displayMode) => window.matchMedia('(display-mode: ' + displayMode + ')').matches,
+        ),
+    );
+  }, []);
+  const TabBar = () => {
+    if (isPwa) {
+      return (
+        <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+          <BottomNavigation showLabels>
+            <BottomNavigationAction label="首页" icon={<Home />} onClick={() => router.push('/')}/>
+            <BottomNavigationAction label="博客" icon={<Home />} onClick={() => router.push('/blogs')}/>
+            <BottomNavigationAction label="文档" icon={<Home />} onClick={() => router.push('/docs')}/>
+            <BottomNavigationAction label="工具" icon={<Home />} onClick={() => router.push('/tools')}/>
+            <BottomNavigationAction label="联系" icon={<Home />} onClick={() => router.push('/contact')}/>
+          </BottomNavigation>
+        </Paper>
+      );
+    }
+    return null;
+  };
   return (
     <>
-      <Header></Header>
+      {!isPwa ? <Header /> : null}
       {children}
+      <TabBar/>
     </>
   );
 }
