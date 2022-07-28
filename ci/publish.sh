7134d4d73e4e5030941cf6b7c0c296e79eb1e6d6
cityhunter <dongjun1997@outlook.com>
2022-07-28 23:25:20 +0800

    fix: https 化

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,20 +1,20 @@
-CONTAINER_ID=`docker ps -a -q -f name=developer-center-web`
-IMAGE_ID=$(docker images | grep "developer-center-web" | awk '{print $3}')
+CONTAINER_ID=`docker ps -a -q -f name=web-evo-site`
+IMAGE_ID=$(docker images | grep "web-evo-site" | awk '{print $3}')
 echo $CONTAINER_ID
-echo "停止 developer-center-web"
+echo "停止 web-evo-site"
 if [ -n "$CONTAINER_ID" ]
 then
   docker stop $CONTAINER_ID
-  echo "删除 developer-center-web 容器"
+  echo "删除 web-evo-site 容器"
   docker rm $CONTAINER_ID
 fi
 if [ -n "$IMAGE_ID" ]
 then
-  echo "删除 developer-center-web 镜像"
+  echo "删除 web-evo-site 镜像"
   docker rmi $IMAGE_ID
 fi
-echo "开始 developer-center-web 镜像打包"
-docker build -t developer-center-web .
-echo "运行 developer-center-web 镜像：4206"
-docker run --name developer-center-web -p 4206:3000 -d developer-center-web
+echo "开始 web-evo-site 镜像打包"
+docker build -t web-evo-site .
+echo "运行 web-evo-site 镜像：4210"
+docker run --name web-evo-site -p 4210:3000 -d web-evo-site
 echo "部署完成"
