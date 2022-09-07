6267230d9643d2012147cd5a49a488405f466a42
cityhunter <dongjun1997@outlook.com>
2022-09-08 02:26:36 +0800

    fix: 课程页面

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,21 +1,55 @@
-import React from 'react';
-import {Box, Container, Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {Box, Container, Grid, Typography} from '@mui/material';
 import Header from '../../components/header';
+import CommonHead from '../../components/common-head';
+import {School} from '@mui/icons-material';
+
 
 /**
  * 课程
  * @constructor
  */
 function CoursesIndex() {
+  const [courses] = useState([
+    {title: '测试1', description: '测试课程1', counter: 1},
+    {title: '测试2', description: '测试课程2', counter: 2},
+    {title: '测试3', description: '测试课程3', counter: 3},
+    {title: 'cycle', description: 'cycle', counter: 4},
+  ]);
   return (
     <Container maxWidth={'md'}>
-      <Header></Header>
-      <Box sx={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+      <CommonHead title={'课程 - WebEvolution'}/>
+      <Header />
+      <Box sx={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap', mb: 9}}>
         <Box sx={{width: '100%', display: 'flex', justifyContent: 'center'}} mb={4}>
           <Typography variant={'h4'}>课程</Typography>
         </Box>
         <Typography variant={'body1'}>探索我们的结构化学习路径，发掘您为现代网络构建时的所有须知事项。</Typography>
       </Box>
+      <Grid container spacing={{xs: 2, md: 3}} columns={12}>
+        {courses.map((it, index) => (
+          <Grid key={index} item xs={12} sm={6} md={4}>
+            <Box sx={{
+              'width': '100%',
+              'border': '1px solid #d2d3d7',
+              'padding': '20px',
+              'boxSizing': 'border-box',
+              'text-decoration': 'none',
+              '&:hover': {
+                border: '1px solid red',
+              },
+            }}>
+              <Box sx={{display: 'flex'}}>
+                <Box sx={{flex: '1 1 auto'}}/>
+                <School sx={{mr: 1}} color={'primary'}/>
+                {it.counter}
+              </Box>
+              <Box sx={{fontSize: 'clamp(1.125rem, 1.09rem + 0.18vw, 1.25rem)'}}>{it.title}</Box>
+              <Box sx={{fontSize: 'clamp(0.75rem, 0.71rem + 0.18vw, 0.875rem);'}}> {it.description}</Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 }
