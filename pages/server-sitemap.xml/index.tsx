5db8fb1a78227d9e922352cf4dfa20177e27d2b7
cityhunter <dongjun1997@outlook.com>
2022-10-15 03:48:12 +0800

    fix: 站点地图

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -6,7 +6,7 @@ const SitemapIndex = () => {};
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const fields: {loc: string, lastmod: string}[] = [];
   try {
-    const host = 'http://developer.bulv.life';
+    const host = 'https://web-evo.bulv.life';
     const url = `${process.env.NEXT_PUBLIC_MIDDLEWARE_URL}blogs`;
     const response = await axios.get(url);
     fields.push(...response.data.map((blog: any) => ({
