434e411ad7d7c781bde38fe42b51259ce3effe81
cityhunter <dongjun1997@outlook.com>
2022-10-14 18:34:42 +0800

    feat: 404返回首页

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
 import {Images} from '../core/libs/images';
-import {Box, Typography} from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
+import {useRouter} from 'next/router';
 const Custom404 = () => {
+  const router = useRouter();
   const pageSx = {
     display: 'flex',
     flexDirection: 'column',
@@ -14,12 +16,16 @@ const Custom404 = () => {
     left: '0',
     right: '0',
   };
+  const backToHome = () => {
+    router.replace('/').then();
+  };
   return (
     <Box sx={pageSx}>
       <Box sx={{width: '260px'}}>
         <Image src={Images.undrawPageNotFound}/>
       </Box>
-      <Typography variant={'h4'}>Page not found</Typography>
+      <Typography variant={'h4'} sx={{mt: 4}}>Page not found</Typography>
+      <Button variant={'contained'} sx={{mt: 4}} onClick={backToHome}>Back to home</Button>
     </Box>
   );
 };
