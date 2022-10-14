15c893c5097d7f6674ccdba7a0f9204ee04b8fe2
cityhunter <dongjun1997@outlook.com>
2022-10-15 03:11:20 +0800

    fix: 部署

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
+});
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-const path = require('path');
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
@@ -15,11 +19,6 @@ const nextConfig = {
     NEXT_PUBLIC_BASE_URL: process.env.BASE_URL,
     NEXT_PUBLIC_SITE_URL: process.env.SITE_URL,
     NEXT_PUBLIC_MIDDLEWARE_URL: process.env.MIDDLEWARE_URL,
-  },
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    runtimeCaching,
   },
   images: {
     domains: [
