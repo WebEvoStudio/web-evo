747b802522e742f518eef344a68d0f3b383a5f68
cityhunter <dongjun1997@outlook.com>
2022-11-13 04:58:10 +0800

    feat: 修改导航栏样式

--- ci/publish.sh
+++ ci/publish.sh
@@ -15,6 +15,6 @@ then
 fi
 echo "开始 web-evo-site 镜像打包"
 docker build -t web-evo-site .
-echo "运行 web-evo-site 镜像：4211"
-docker run --name web-evo-site -p 4211:3000 -d web-evo-site
+echo "运行 web-evo-site 镜像：4210"
+docker run --name web-evo-site -p 4210:3000 -d web-evo-site
 echo "部署完成"
