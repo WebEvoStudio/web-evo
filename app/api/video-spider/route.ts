b0140c55c479154f59cc4a3d64d3decb633277f7
cityhunter <dongjun1997@outlook.com>
2023-12-15 21:51:31 +0800

    fix: 微博视频解析

--- app/api/video-spider/route.ts
+++ app/api/video-spider/route.ts
@@ -13,6 +13,7 @@ const getV = async (src: string) => {
     body: formData,
     method: 'POST',
     headers: {
+      // eslint-disable-next-line max-len
       'Cookie': 'SUB=_2AkMSIKJxf8NxqwFRmfoWzmjibIhzwgnEieKkfFOqJRMxHRl-yT9kqmgrtRB6OaCMng8wuZIVAUk1aKBpykgEItg9Mfn9; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9Wh00YNYJdbp6Kzr71bx_9ws; _s_tentry=passport.weibo.com; Apache=145601819250.38495.1702636872301; SINAGLOBAL=145601819250.38495.1702636872301; ULV=1702636872440:1:1:1:145601819250.38495.1702636872301:; XSRF-TOKEN=K6cu6I1U9UGbWYHYFEurg2Sw; WBPSESS=V0zdZ7jH8_6F0CA8c_ussdvQGhR2QXeIJJgFME4e84pBnEH2olSc9hgE_oIsxJNVn6wXPK7yBbro6t82JCm5fJJ29BalrJBGpH8bKnw-kqGbM7p7ZMVIrS-yf0Tjiiwr',
       'Referer': `https://weibo.com/tv/show/${id}?from=old_pc_videoshow`,
     },
